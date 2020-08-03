6e8dfd11e3add2f6b4a4b603e1fc20b3d20a51cb
Tommy Long Pham <tpham@smugmug.com>
2020-08-03 14:51:41 -0700

    test creation

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,4 +1,19 @@
 import { crudControllers } from '../../utils/crud'
 import { Item } from './item.model'
+import { connect } from '../../utils/db';
+import mongoose from 'mongoose';
+
+const run = async () => {
+    await connect('mongodb://localhost:27017/api-test');
+    const item = await Item.create({
+        name: 'Clean up',
+        createdBy: mongoose.Types.ObjectId(),
+        list: mongoose.Types.ObjectId(),
+    });
+
+    console.log(item);
+};
+
+run();
 
 export default crudControllers(Item)
