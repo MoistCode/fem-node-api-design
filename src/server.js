7b6c7814ecf29eaed4df62e584eca1b197159f9c
Tommy Long Pham <tpham@smugmug.com>
2020-08-03 13:17:35 -0700

    lesson 1

--- src/server.js
+++ src/server.js
@@ -5,12 +5,39 @@ import cors from 'cors'
 
 export const app = express()
 
+const router = express.Router();
+
 app.disable('x-powered-by')
 
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+
+router.get('/me', (req, res) => {
+    res.send({
+        me: 'In the router for /me',
+    });
+});
+
+const routes = [
+    'get /cat/',
+    'get /cat/:id',
+    'post /cat',
+    'put /cat/:id',
+    'delete /cat/:id'
+];
+
+router.route('/cat')
+    .get()
+    .post();
+
+router.route('/cat/:id')
+    .get()
+    .put()
+    .delete();
+
+app.use('/api', router);
 
 const log = (req, res, next) => {
     console.log(`Logging request body: ${JSON.stringify(req.body)}`);
@@ -31,6 +58,14 @@ app.get('/data', [log, log, log], (req, res) => {
     res.send({
         data: [1, 2, 3],
     });
+});
+
+app.put('/data', (req, res) => {
+
+});
+
+app.delete('/data', (req, res) => {
+
 });
 
 app.post('/data', (req, res) => {
