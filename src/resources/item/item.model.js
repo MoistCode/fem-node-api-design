c8146abcf9e861df41af5bb3bc87ecb3ef141ecb
Tommy Long Pham <tpham@smugmug.com>
2020-08-03 14:31:56 -0700

    indexing

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -16,16 +16,24 @@ const itemSchema = new mongoose.Schema({
     notes: String,
     due: Date,
     createdBy: {
-        type: mongoose.Types.ObjectId,
+        type: mongoose.SchemaTypes.ObjectId,
         ref: 'user',
         required: true,
     },
     list: {
-        type: mongoose.Types.ObjectId,
+        type: mongoose.SchemaTypes.ObjectId,
         ref: 'list',
         required: true,
     },
 }, {
     timestamps: true
-})
+});
+
+itemSchema.index({
+    list: 1,
+    name: 1
+}, {
+    unique: true
+});
+
 export const Item = mongoose.model('item', itemSchema)
