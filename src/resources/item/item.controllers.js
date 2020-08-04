8c4bff7d43b405eef0399441ad1638bb981c932e
Tommy Long Pham <tpham@smugmug.com>
2020-08-04 05:57:29 -0700

    lesson 3

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,17 +1,34 @@
-import { crudControllers } from '../../utils/crud'
-import { Item } from './item.model'
-import { connect } from '../../utils/db';
+import {
+    crudControllers
+} from '../../utils/crud'
+import {
+    Item
+} from './item.model'
+import {
+    connect
+} from '../../utils/db';
 import mongoose from 'mongoose';
 
 const run = async () => {
     await connect('mongodb://localhost:27017/api-test');
+
     const item = await Item.create({
         name: 'Clean up',
         createdBy: mongoose.Types.ObjectId(),
         list: mongoose.Types.ObjectId(),
     });
 
-    console.log(item);
+    const updated = Item.findByIdAndUpdate(item._id, {
+        name: 'eat'
+    }, {
+        new: true
+    }).exec();
+
+    const deleted = Item.findByIdAndDelete(item._id).exec();
+
+    console.log(await updated);
+    console.log(await deleted);
+    console.log(await Item.findById(item._id));
 };
 
 run();
